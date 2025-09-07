00923ff8a05960ec4ec276d038d27c1a2dedf1df
fadd00 <andhikahutama9@gmail.com>
2025-09-07 17:30:31 +0700

    feat: Update components for improved structure and styling
    - Refactored Contact component for better readability and maintainability.
    - Enhanced Experience component to display work history with improved layout.
    - Updated Footer component to ensure consistent styling and links.
    - Refined Header component for better navigation and scroll behavior.
    - Improved Hero component with engaging content and smooth scrolling functionality.
    - Enhanced Projects component to showcase work with better design and links.
    - Added next-sitemap configuration for SEO optimization.
    - Created robots.txt for search engine indexing.
    - Initialized sitemap.xml for better site structure visibility.

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://andhu.tech', // Ganti dengan domain kamu
+  generateRobotsTxt: true, // (optional) Generate robots.txt juga
+};
